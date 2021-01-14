b627ab0c8e56ad27c187a70a531a550ef3a79849
Paul Reeve <preeve@pdjr.eu>
2021-01-14 15:00:58 +0100

    Fix: use new SignalkClient

--- SwitchMonitorWidget.js
+++ SwitchMonitorWidget.js
@@ -10,17 +10,19 @@ class SwitchMonitorWidget {
     this.switchbanks = { "misc": [] };
 
     var switchPaths = new Set();
-    signalkClient.getAvailablePathsSync("^electrical\.switches\.bank\.(.+)\.(.+)\.state\.meta\.type$").forEach(path => {
-      console.log("%s %s", path, path.substr(0, (path.length - 6)));
-    });
-    switchBankPaths.forEach(key => {
-      var sbmatch = key.match(/^.*\.(.+)\.(\d+)\.state$/);
-      if ((sbmatch) && (sbmatch.length == 3)) {
-        console.log(sbmatch[1]);
-        if (!this.switchbanks.hasOwnProperty('' + sbmatch[1])) this.switchbanks['' + sbmatch[1]] = [];
-        this.switchbanks['' + sbmatch[1]].push(key);
-      } else {
-        this.switchbanks['misc'].push(key);
+    signalkClient.getAvailablePathsSync("^electrical\.switches\.bank\.(.+)\.(\\d+)\.state$").forEach(path => {
+      path = path.substr(0, (path.length - 6));
+      var meta = signalkClient.getValueSync(path + ".state.meta", (v) => v);
+      if ((meta) && (meta.shortName)) {
+        var sbmatch = path.match(/^.*\.(.+)\.(\d+)$/);
+        if ((sbmatch) && (sbmatch.length == 3)) {
+          var instance = "" + sbmatch[1];
+          var index = parseInt(sbmatch[2]);
+          if (!this.switchbanks.hasOwnProperty(instance)) this.switchbanks[instance] = [];
+          this.switchbanks[instance].push({ path: path, meta: meta });
+        } else {
+          this.switchbanks['misc'].push({ path: path, meta: meta });
+        }
       }
     });
 
@@ -30,7 +32,7 @@ class SwitchMonitorWidget {
     });
   }
 
-  makeSwitchBank(instance, channels) {
+  makeSwitchBank(instance, channel) {
     //instance = (Number.isNaN(parseInt(instance, 10)))?instance:parseInt(instance, 10);
     var switchbankContainer = PageUtils.createElement('div', null, 'switchbank-container' + ((instance == 'misc')?'':'hidden'), null, null);
     var switchbankTable = PageUtils.createElement('div', null, 'table switchbank-table', null, switchbankContainer);
@@ -39,29 +41,26 @@ class SwitchMonitorWidget {
     var switchbankTableChannelContainer = PageUtils.createElement('div', null, 'table-cell switchbank-table-channel-container', null, switchbankTableRow); 
     var switchbankChannelTable = PageUtils.createElement('div', null, 'table switchbank-channel-table', null, switchbankTableChannelContainer);
     var switchbankChannelTableRow = PageUtils.createElement('div', null, 'table-row switchbank-channel-table-row', null, switchbankChannelTable);
-    channels.forEach(key => {
-      var path = "electrical.switches.bank." + key;
-      var switchbankChannelCell = PageUtils.createElement('div', 'CH' + key, 'table-cell switchbank-channel-cell artifact', null, switchbankChannelTableRow);
+    channel.forEach(channel => {
+      var switchbankChannelCell = PageUtils.createElement('div', channel.path, 'table-cell switchbank-channel-cell artifact', null, switchbankChannelTableRow);
       switchbankChannelCell.addEventListener('click', function(e) { this.operateSwitch(e.currentTarget.id.substr(2), e.currentTarget.classList.contains('on')); }.bind(this));
-      var channelId = (key.includes('.'))?key.slice(key.lastIndexOf('.') + 1):key;
+      var channelId = (channel.path.includes('.'))?channel.path.slice(channel.path.lastIndexOf('.') + 1):channel.path;
       var switchbankChannelCellKey = PageUtils.createElement('span', null, 'key hidden', document.createTextNode(channelId), switchbankChannelCell);  
       var switchbankChannelCellName = PageUtils.createElement('span', null, 'name', document.createTextNode(channelId), switchbankChannelCell);  
       if (Number.isNaN(parseInt(instance, 10))) switchbankChannelCell.classList.remove('artifact');
 
-      this.signalkClient.getValue(path + '.state.meta', function(sbc, v) {
-        sbc.classList.remove('artifact'); switchbankContainer.classList.remove('hidden');
-        if (v.type) sbc.classList.add(v.type);
-        if (v.displayName) {
-          sbc.querySelector('.name').innerHTML = v.displayName;
-          sbc.querySelector('.key').classList.remove('hidden');
-        }
-      }.bind(this, switchbankChannelCell), (v) => v);
+      switchbankChannelCell.classList.remove('artifact'); switchbankContainer.classList.remove('hidden');
+      if (channel.meta.type) switchbankChannelCell.classList.add(channel.meta.type);
+      if (channel.meta.displayName) {
+        switchbankChannelCell.querySelector('.name').innerHTML = channel.meta.displayName;
+        switchbankChannelCell.querySelector('.key').classList.remove('hidden');
+      }
             
-      this.signalkClient.registerCallback(path + '.state', function(sbc,v) {
+      this.signalkClient.onValue(channel.path + ".state", function(sbc,v) {
         var millis = Date.UTC() - Date.parse(v.timestamp);
         if (millis > 150000) sbc.classList.add('expired'); else sbc.classList.remove('expired');
         if (v.value) { sbc.classList.add('on'); sbc.classList.remove('off'); } else { sbc.classList.add('off'); sbc.classList.remove('on'); }
-      }.bind(this, switchbankChannelCell), (v) => v);
+      }.bind(this, switchbankChannelCell), (v) => v, false);
 
     });
     return(switchbankContainer);
